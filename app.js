e54360391ebbb32cbb0cc91db2b0a7b266eff0f9
Rithik Sachdev <rithiksachdev3@gmail.com>
2019-09-19 00:29:01 +0530

    Beginning of GUI

---
+++ app.js
@@ -0,0 +1,32 @@
+var express=require('express');
+
+var app=express();
+
+app.set('view engine','ejs');
+
+app.use(require('body-parser').urlencoded({extended : true}));
+
+app.set('port',process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/',function(req,res){
+    res.render('home');
+});
+
+app.get('/login',function(req,res){
+  res.render('login');
+});
+
+
+
+app.use(function(req,res,next){
+    console.log("looking for url"+ req.url);
+});
+
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + ' press Ctrl-C to terminate');
+});
+
